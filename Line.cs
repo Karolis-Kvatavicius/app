e25c129fce0b31571c2c6edb90fcf90a4bb0a571
Karolis-Kvatavicius <karolis.kvatavicius@gmail.com>
2022-11-29 11:56:03 +0200

    Initial commit

---
+++ Line.cs
@@ -0,0 +1,47 @@
+public class Line
+{
+    public string deviceVendor { get; set; }
+    public string deviceProduct { get; set; }
+    public string deviceVersion { get; set; }
+    public string signatureId { get; set; }
+    public string severity { get; set; }
+    public string name { get; set; }
+    public string start { get; set; }
+    public string rt { get; set; }
+    public string msg { get; set; }
+    public string shost { get; set; }
+    public string smac { get; set; }
+    public string dhost { get; set; }
+    public string dmac { get; set; }
+    public string suser { get; set; }
+    public string suid { get; set; }
+    public string externalId { get; set; }
+    public string cs1Label { get; set; }
+    public string cs1 { get; set; }
+
+    public static Line ParseRecordFromLine(string line)
+    {
+        string[] parts = line.Split(',');
+        return new Line
+        {
+            deviceVendor = parts[0],
+            deviceProduct = parts[1],
+            deviceVersion = parts[2],
+            signatureId = parts[3],
+            severity = parts[4],
+            name = parts[5],
+            start = parts[6],
+            rt = parts[7],
+            msg = parts[8],
+            shost = parts[9],
+            smac = parts[10],
+            dhost = parts[11],
+            dmac = parts[12],
+            suser = parts[13],
+            suid = parts[14],
+            externalId = parts[15],
+            cs1Label = parts[16],
+            cs1 = parts[17]
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+Console.WriteLine("Type column name:");
+string column = Console.ReadLine();
+
+Console.WriteLine("Type search query:");
+string query = Console.ReadLine();
+
+var record = File.ReadLines("20220601182758.csv").Skip(1)
+                 .Select(Line.ParseRecordFromLine)
+                 .Where(rec => ((string)typeof(Line).GetProperty(column).GetValue(rec)).Contains(query))
+                 .AsEnumerable();
+
+if (record != null || record.Any())
+{
+    foreach (var item in record)
+    {
+        string jsonString = JsonSerializer.Serialize(item);
+        Console.WriteLine(jsonString);
+    }
+}
+else
+{
+    Console.WriteLine("No records found");
+}
